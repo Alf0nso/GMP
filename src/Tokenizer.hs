e66e1d4fbe4a5210b6f6e523c39f972bbe07f224
Afonso Rafael <afonsorafael@sapo.pt>
2023-08-19 16:52:48 +0200

    Adding simple tokenizer to the library, to be used afterwards.

---
+++ src/Tokenizer.hs
@@ -0,0 +1,45 @@
+module Tokenizer
+  ( Token(..)
+  , tokenizer
+  ) where
+{-
+Building a tokenizer to use afterwards with the parser.
+-}
+
+{- Position related -}
+type Position = (Integer, Integer)
+
+incrX :: Position -> Position
+incrX = incrXx 1
+
+incrXx :: Integer -> Position -> Position
+incrXx i (x, y) = (x + i, y)
+
+incrY :: Position -> Position
+incrY = incrYy 1
+
+incrYy :: Integer -> Position -> Position
+incrYy i (x, y) = (x, y + i)
+------------------------------
+
+
+{- Token related -}
+data Token    = Token Char Position
+              deriving (Show)
+
+{- Instances -}
+instance Eq Token where
+  Token c1 _ == Token c2 _ = c1 == c2
+  Token c1 _ /= Token c2 _ = c1 /= c2
+------------------------------
+
+tokenizer' :: Position -> String -> [Token]
+tokenizer' _   []            = []
+tokenizer' pos (' ':string)  = tokenizer' (incrX pos)    string
+tokenizer' pos ('\t':string) = tokenizer' (incrXx 4 pos) string
+tokenizer' pos ('\n':string) = tokenizer' (incrY pos)    string
+tokenizer' pos (c:string)    =
+  (Token c pos):tokenizer' (incrX pos) string
+
+tokenizer :: String -> [Token]
+tokenizer = tokenizer' (1,1)
