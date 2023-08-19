03bc8b8e6692534f864f35feaef5094d02de9d87
Afonso Rafael <afonsorafael@sapo.pt>
2023-08-20 01:12:38 +0200

    Definition of token parser.
    Which can fail with a specific error.

--- src/Parser.hs
+++ src/Parser.hs
@@ -1,21 +1,32 @@
 module Parser
   ( Parser(..)
+  , token
   , satisfy
   , char
   , string
   ) where
 
 import Prelude hiding (any)
+import Control.Applicative (Alternative (..))
+import Data.List (nub)
 
 {- Types and Data types -}
 data Error i = EndOfInput
              | Unexpected i
+             | Expected i i
              | Empty
              deriving (Show, Eq)
 
 newtype Parser i o =
   Parser { parse :: [i] -> Either [Error i] (o, [i]) }
                      deriving (Functor)
+
+token :: (i -> Error i) -> (i -> Bool) -> Parser i i
+token err predicate = Parser $ \input -> case input of
+  [] -> Left [EndOfInput]
+  test : rest
+    | predicate test -> Right (test, rest)
+    | otherwise      -> Left [err test]
 ------------------------------------------------------------
 
 {- Instances -}
@@ -38,7 +49,18 @@ instance Monad (Parser i) where
       Left err -> Left err
       Right (out, rest) ->
         let Parser pars' = cont out in pars' rest
+
+instance (Eq i) => Alternative (Parser i) where
+  empty = Parser $ \_ -> Left [Empty]
+
+  Parser p1 <|> Parser p2 = Parser $ \input ->
+    case p1 input of
+      Left err -> case p2 input of
+                   Left err'         -> Left $ nub $ err <> err'
+                   Right (out, rest) -> Right (out, rest)
+      Right (out, rest) -> Right (out, rest)
 ------------------------------------------------------------
+
 
 {- Elementary Parsers -}
 satisfy :: (i -> Bool) -> Parser i i
@@ -54,3 +76,4 @@ char i = satisfy (== i)
 string :: Eq i => [i] -> Parser i [i]
 string []  = pure []
 string (c:str) = (:) <$> char c <*> string str
+------------------------------------------------------------
