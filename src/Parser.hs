33290620f06433f4abe39fee7a3302f1a80d0fe1
Afonso Rafael <afonsorafael@sapo.pt>
2023-08-22 20:42:18 +0200

    Change instance of show for errors of the parser.

--- src/Parser.hs
+++ src/Parser.hs
@@ -28,7 +28,22 @@ data Error i = EndOfInput
              | Expected i i
              | Empty
              | NoMatch String
-             deriving (Show, Eq)
+             deriving (Eq)
+
+instance (Show i) => Show (Error i) where
+  show (EndOfInput) = "End of input"
+  show (ExpectedEndOfInput i) =
+    "Expected end of input but got " ++ show i
+  show (ExpectedSomething) =
+    "Expected something"
+  show (Unexpected i) =
+    "Unexpected " ++ show i
+  show (Expected i1 i2) =
+    "Expected " ++ show i1 ++
+    " but got " ++ show i2
+  show (Empty) = "Empty"
+  show (NoMatch str) =
+    "No match for " ++ str
 
 newtype Parser i o =
   Parser { parse :: [i] -> Either [Error i] (o, [i]) }
