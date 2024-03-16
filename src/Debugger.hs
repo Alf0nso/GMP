cd139aa9c71b5768366d7ac2a193d09e26de2565
Afonso Rafael <afonsorafael@sapo.pt>
2024-03-16 11:32:28 +0100

    Apply the error pretty printer to the debugger.

--- src/Debugger.hs
+++ src/Debugger.hs
@@ -4,13 +4,14 @@ module Debugger
   , solveParser ) where
 
 import Parser ( Parser(..) )
-import Errors ( Error(..) )
+import Errors ( Error(..)
+              , printErrors )
 
 debuggerParse :: (Show t, Show p) => [t] -> Parser t p -> IO ()
 debuggerParse tokens parser = solveParser $ parse parser tokens
 
 solveParser :: (Show t, Show p) => Either [Error t] (p, [t]) -> IO ()
 solveParser = \case
-  Left err             -> print err
+  Left err             -> putStrLn $ printErrors err
   Right (parsed, left) -> putStrLn $ "Parsing output: " ++ show parsed
                          ++ "\n" ++ "Left: " ++ show left
