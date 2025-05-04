1e5f6edfc06dae0c90579c8dfc910663783fe839
Afonso Rafael <afonsorafael@sapo.pt>
2025-05-04 20:27:35 +0200

    Change[Basics]: So it works with the new tokenizer

--- src/Basics.hs
+++ src/Basics.hs
@@ -34,31 +34,34 @@ import Debugger
   , debuggerParseReturn
   )
 
+type Tchar   = Token Char
+type Tstring = [Token Char]
+
 {- Basic token functions -}
-space, digit, letter :: Parser Token Token
+space, digit, letter :: Parser Tchar Tchar
 space  = satisfy isSpace
 digit  = satisfy isDigit
 letter = satisfy isLetter
 
-spaces, digits, letters :: Parser Token [Token]
+spaces, digits, letters :: Parser Tchar Tstring
 spaces  = many space
 digits  = many1 digit
 letters = many1 letter
 
-charSymbol :: Char -> Parser Token Token
+charSymbol :: Char -> Parser Tchar Tchar
 charSymbol chr = symbol chr offToken <* spaces
 
-stringSymbols :: String -> Parser Token [Token]
+stringSymbols :: String -> Parser Tchar Tstring
 stringSymbols str = string (tokenString str) <* spaces
 ------------------------------------------------------------
 
 {- Executing parsers -}
-executeParserD :: Show p => Parser Token p -> String -> IO ()
+executeParserD :: Show p => Parser Tchar p -> String -> IO ()
 executeParserD parser str = debuggerParse (tokenizer str) parser
 
-executeParserDR :: Show p => Parser Token p -> String -> IO (Maybe p)
+executeParserDR :: Show p => Parser Tchar p -> String -> IO (Maybe p)
 executeParserDR parser str = debuggerParseReturn (tokenizer str) parser
 
-executeParserDWR :: ((p, [Token]) -> IO ()) -> Parser Token p -> String -> IO ()
+executeParserDWR :: ((p, Tstring) -> IO ()) -> Parser Tchar p -> String -> IO ()
 executeParserDWR fun parser str = debuggerParseWR fun (tokenizer str) parser
 ------------------------------------------------------------------
